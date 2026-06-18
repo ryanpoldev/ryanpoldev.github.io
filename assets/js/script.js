0a9cc6e3c8269a7e41a0737a6143316302bbc909
ryanpoldev <ryanpolgit@gmail.com>
2026-06-19 01:42:16 +0800

    add tooltip for icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,41 +35,46 @@ avatar.addEventListener("mouseleave", () => {
 
 
 document.querySelectorAll('.uiworks-custom').forEach(img => {
-    img.addEventListener('click', function () {
-        document.getElementById('modalImage').src = this.src;
-    });
+  img.addEventListener('click', function () {
+    document.getElementById('modalImage').src = this.src;
+  });
 });
 
 document.querySelectorAll('.cert-card').forEach(card => {
-    card.addEventListener('click', function () {
-        document.getElementById('modalImage').src = this.dataset.certImg;
-    });
+  card.addEventListener('click', function () {
+    document.getElementById('modalImage').src = this.dataset.certImg;
+  });
 });
 
 /*clock*/
 function updateClock() {
-            const now = new Date();
-            
-            // Format options to match "Saturday 5:55 PM"
-            const options = { 
-                weekday: 'long', 
-                hour: 'numeric', 
-                minute: '2-digit', 
-                hour12: true 
-            };
-            
-            // Generate the formatted string based on the user's local time
-            const timeString = now.toLocaleDateString('en-US', options);
-            
-            // Clean up the string format (toLocaleDateString often adds a comma after the day)
-            const cleanTimeString = timeString.replace(',', '');
-            
-            // Inject the time into the HTML element
-            document.getElementById('live-clock').textContent = cleanTimeString;
-        }
+  const now = new Date();
 
-        // Run the clock function immediately when the page loads
-        updateClock();
+  // Format options to match "Saturday 5:55 PM"
+  const options = {
+    weekday: 'long',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true
+  };
 
-        // Update the clock every second (1000 milliseconds) to keep it accurate
-        setInterval(updateClock, 1000);
+  // Generate the formatted string based on the user's local time
+  const timeString = now.toLocaleDateString('en-US', options);
+
+  // Clean up the string format (toLocaleDateString often adds a comma after the day)
+  const cleanTimeString = timeString.replace(',', '');
+
+  // Inject the time into the HTML element
+  document.getElementById('live-clock').textContent = cleanTimeString;
+}
+
+// Run the clock function immediately when the page loads
+updateClock();
+
+// Update the clock every second (1000 milliseconds) to keep it accurate
+setInterval(updateClock, 1000);
+
+/*tooltip*/
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(el => {
+  new bootstrap.Tooltip(el);
+});
